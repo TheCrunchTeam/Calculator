0c0fe5f0552cf20e7bf0e07e69089d82746b0773
fedefrote <fede_imfeld@hotmail.com>
2017-09-07 10:57:43 -0300

    marcos

---
+++ oBasico.py
@@ -0,0 +1,18 @@
+import time
+
+def sumar(*args):
+	result = 0
+	for valor in args:
+		result += valor
+	return result
+
+def multiplicar(*args):
+	result = 1
+	for valor in args:
+		result *= valor
+	return result
+
+
+
+
+time.sleep(0.2)
